786307f51ddbe3042ff5ff96069a014736fa883a
MaRiaAbbas123146 <221749669+MaRiaAbbas123146@users.noreply.github.com>
2026-08-09 16:23:02 +0500

    add conversation model and controller

--- frontend/src/components/Shop/DashboardMessages.jsx
+++ frontend/src/components/Shop/DashboardMessages.jsx
@@ -301,12 +301,12 @@ const MessageList = ({
         <img
           src={`${user?.avatar?.url}`}
           alt=""
-          className="w-[50px] h-[50px] rounded-full"
+          className="w-12.5 h-12.5 rounded-full"
         />
         {online ? (
-          <div className="w-[12px] h-[12px] bg-green-400 rounded-full absolute top-[2px] right-[2px]" />
+          <div className="w-3 h-3 bg-green-400 rounded-full absolute top-0.5 right-0.5" />
         ) : (
-          <div className="w-[12px] h-[12px] bg-[#c7b9b9] rounded-full absolute top-[2px] right-[2px]" />
+          <div className="w-3 h-3 bg-[#c7b9b9] rounded-full absolute top-0.5 right-0.5" />
         )}
       </div>
       <div className="pl-3">
@@ -342,10 +342,10 @@ const SellerInbox = ({
           <img
             src={`${userData?.avatar?.url}`}
             alt=""
-            className="w-[60px] h-[60px] rounded-full"
+            className="w-15 h-15 rounded-full"
           />
           <div className="pl-3">
-            <h1 className="text-[18px] font-[600]">{userData?.name}</h1>
+            <h1 className="text-[18px] font-semibold">{userData?.name}</h1>
             <h1>{activeStatus ? "Active Now" : ""}</h1>
           </div>
         </div>
@@ -369,21 +369,21 @@ const SellerInbox = ({
                 {item.sender !== sellerId && (
                   <img
                     src={`${userData?.avatar?.url}`}
-                    className="w-[40px] h-[40px] rounded-full mr-3"
+                    className="w-10 h-10 rounded-full mr-3"
                     alt=""
                   />
                 )}
                 {item.images && (
                   <img
                     src={`${item.images?.url}`}
-                    className="w-[300px] h-[300px] object-cover rounded-[10px] mr-2"
+                    className="w-75 h-75 object-cover rounded-[10px] mr-2"
                   />
                 )}
                 {item.text !== "" && (
                   <div>
                     <div
-                      className={`w-max p-2 rounded ${item.sender === sellerId ? "bg-[#000]" : "bg-[#38c776]"
-                        } text-[#fff] h-min`}
+                      className={`w-max p-2 rounded ${item.sender === sellerId ? "bg-black" : "bg-[#38c776]"
+                        } text-white h-min`}
                     >
                       <p>{item.text}</p>
                     </div>
@@ -404,7 +404,7 @@ const SellerInbox = ({
         className="p-3 relative w-full flex justify-between items-center"
         onSubmit={sendMessageHandler}
       >
-        <div className="w-[30px]">
+        <div className="w-7.5">
           <input
             type="file"
             name=""
